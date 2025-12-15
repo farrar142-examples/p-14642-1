217544b4f6dec3d9621d3458dd2bcf4a64426b18
farrar142 <gksdjf1690@gmail.com>
2025-12-15 22:48:27 +0900

    c10

---
+++ src/main/java/com/back/domain/post/post/documents/Post.java
@@ -0,0 +1,26 @@
+package com.back.domain.post.post.documents;
+
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.elasticsearch.annotations.Document;
+import org.springframework.data.elasticsearch.annotations.Field;
+import org.springframework.data.elasticsearch.annotations.FieldType;
+
+import java.time.LocalDateTime;
+
+@Document(indexName = "posts")
+public class Post {
+    @Id
+    private String id;
+    @Field(type= FieldType.Text)
+    private String title;
+    @Field(type= FieldType.Text)
+    private String content;
+    @Field(type= FieldType.Keyword)
+    private String author;
+    @CreatedBy
+    private LocalDateTime createdAt;
+    @LastModifiedBy
+    private LocalDateTime updatedAt;
+}
